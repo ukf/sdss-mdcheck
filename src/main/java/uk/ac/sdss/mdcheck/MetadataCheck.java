b5188a5370f3ae0ddd7bd5bfef90b9d64d59c94e
Ian Young <ian@iay.org.uk>
2013-06-14 18:27:14 +0100

    Add standard checkstyle configuration.  Resolve some issues.

--- src/main/java/uk/ac/sdss/mdcheck/MetadataCheck.java
+++ src/main/java/uk/ac/sdss/mdcheck/MetadataCheck.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 University of Edinburgh.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.sdss.mdcheck;
 
 import java.io.File;
@@ -34,199 +50,197 @@ import org.xml.sax.SAXException;
  *
  */
 public class MetadataCheck implements Runnable {
-	
-	private final DocumentBuilderFactory dbFactory;
-	private final String inputFileName;
-	private final Document inputDoc;
-	
-	private final TransformerFactory tFactory;
-	
-	private class Checker {
-		/** File object representing the stylesheet */
-		private final File file;
-		
-		/** System identifier corresponding to the file. */
-		private final String systemID;
-		
-		/** DOM tree form of the stylesheet */
-		private final Document checkDoc;
-		
-		/**
-		 * Get a simple name for this stylesheet for use in error messages.
-		 * 
-		 * @return a simple name for the Checker
-		 */
-		private String getName() {
-			return this.file.getName();
-		}
+    
+    private final DocumentBuilderFactory dbFactory;
+    private final String inputFileName;
+    private final Document inputDoc;
+    
+    private final TransformerFactory tFactory;
+    
+    private class Checker {
+        /** File object representing the stylesheet. */
+        private final File file;
+        
+        /** System identifier corresponding to the file. */
+        private final String systemID;
+        
+        /** DOM tree form of the stylesheet. */
+        private final Document checkDoc;
+        
+        Checker(String checkFileName) throws SAXException, IOException, ParserConfigurationException {
+            this.file = new File(checkFileName);
+            this.checkDoc = buildDocument(checkFileName);
+            this.systemID = this.file.toURI().toString();
+        }
 
-		/**
-		 * Generates a JAXP Source for this stylesheet.  The Source's
-		 * system ID is set to allow resolution of relative URIs within the
-		 * transform.
-		 * 
-		 * @return a JAXP Source object.
-		 */
-		private Source getSource() {
-			return new DOMSource(this.checkDoc, this.systemID);
-		}
+        /**
+         * Get a simple name for this stylesheet for use in error messages.
+         * 
+         * @return a simple name for the Checker
+         */
+        private String getName() {
+            return this.file.getName();
+        }
 
-		Checker(String checkFileName) throws SAXException, IOException, ParserConfigurationException {
-			this.file = new File(checkFileName);
-			this.checkDoc = buildDocument(checkFileName);
-			this.systemID = this.file.toURI().toString();
-		}
-	}
-	
-	private List<Checker> checkers = new ArrayList<Checker>();
-	
-	/**
-	 * Global count of errors across all checks.
-	 */
-	private int errors = 0;
+        /**
+         * Generates a JAXP Source for this stylesheet.  The Source's
+         * system ID is set to allow resolution of relative URIs within the
+         * transform.
+         * 
+         * @return a JAXP Source object.
+         */
+        private Source getSource() {
+            return new DOMSource(this.checkDoc, this.systemID);
+        }
 
-	private static void croak(String s, Exception e) {
-		e.printStackTrace();
-		System.err.println("Internal error: " + s + ": " + e.getMessage());
-		System.exit(1);
-	}
+    }
+    
+    private List<Checker> checkers = new ArrayList<Checker>();
+    
+    /**
+     * Global count of errors across all checks.
+     */
+    private int errors = 0;
 
-	private class MyErrorListener implements ErrorListener {
-		
-		private final String inputFileName;
-		private final String checkerName;
-		private boolean first = true;
+    /**
+     * Constructor.
+     * 
+     * @param inputFileName name of the file containing the metadata
+     *                         to be checked
+     * @param checkFileName name of the file containing the stylesheet
+     *                         that will perform the checking
+     * @throws ParserConfigurationException 
+     * @throws IOException 
+     * @throws SAXException 
+     */
+    private MetadataCheck(String inputFileName)
+        throws SAXException, IOException, ParserConfigurationException {
+        /*
+         * Create a transformer factory.
+         */
+        tFactory = TransformerFactory.newInstance();
+        
+        /*
+         * Create a document builder factory.
+         */
+        dbFactory = DocumentBuilderFactory.newInstance();
+        dbFactory.setNamespaceAware(true);
+        // dbFactory.setValidating(true);
+        
+        /*
+         * Build documents for each of the input files.
+         */
+        this.inputFileName = inputFileName;
+        inputDoc = buildDocument(inputFileName);
+    }
+    
+    private static void croak(String s, Exception e) {
+        e.printStackTrace();
+        System.err.println("Internal error: " + s + ": " + e.getMessage());
+        System.exit(1);
+    }
 
-		private void recordError(TransformerException e)
-		{
-			String msg = e.getMessage();
-			if (first) {
-				first = false;
-				System.err.println("*** checking " + inputFileName +
-						" with " + checkerName);
-			}
-			System.err.println(msg);
-			if (msg.startsWith("[ERROR]")) errors++;
-		}
-		
-		public void error(TransformerException exception)
-				throws TransformerException {
-			recordError(exception);
-		}
+    private class MyErrorListener implements ErrorListener {
+        
+        private final String inputFileName;
+        private final String checkerName;
+        private boolean first = true;
 
-		public void fatalError(TransformerException exception)
-				throws TransformerException {
-			recordError(exception);
-		}
+        MyErrorListener(String inputFileName, String checkerName) {
+            this.inputFileName = new File(inputFileName).getName(); // last component of name only
+            this.checkerName = checkerName;
+        }
 
-		public void warning(TransformerException exception)
-				throws TransformerException {
-			recordError(exception);
-		}
-		
-		MyErrorListener(String inputFileName, String checkerName) {
-			this.inputFileName = new File(inputFileName).getName(); // last component of name only
-			this.checkerName = checkerName;
-		}
-	}
-	
-	private Document buildDocument(String fileName)
-		throws SAXException, IOException, ParserConfigurationException
-	{
-		DocumentBuilder db = dbFactory.newDocumentBuilder();
-		return db.parse(fileName);
-	}
-	
-	private void performCheck(Document input, String inputFileName, Checker checker)
-		throws TransformerException
-	{
-		Transformer t = tFactory.newTransformer(checker.getSource());
-		t.setErrorListener(new MyErrorListener(inputFileName, checker.getName()));
-		t.transform(new DOMSource(input), new DOMResult());
-	}
-	
-	public void run() {
-		try {
-			for (Checker check: checkers) {
-				performCheck(inputDoc, inputFileName, check);
-			}
-		} catch (TransformerConfigurationException e) {
-			croak("transformer configuration exception", e);
-		} catch (TransformerException e) {
-			croak("transformer exception", e);
-		}
-		if (errors != 0) {
-			System.err.println("*** ERRORS ENCOUNTERED IN " +
-					inputFileName + " ***");
-			System.exit(1);
-		}
-	}
+        private void recordError(TransformerException e) {
+            String msg = e.getMessage();
+            if (first) {
+                first = false;
+                System.err.println("*** checking " + inputFileName +
+                        " with " + checkerName);
+            }
+            System.err.println(msg);
+            if (msg.startsWith("[ERROR]")) errors++;
+        }
+        
+        public void error(TransformerException exception)
+                throws TransformerException {
+            recordError(exception);
+        }
 
-	private void addChecker(String checkFileName) throws SAXException, IOException, ParserConfigurationException {
-		checkers.add(new Checker(checkFileName));
-	}
-	
-	/**
-	 * Constructor.
-	 * 
-	 * @param inputFileName name of the file containing the metadata
-	 * 						to be checked
-	 * @param checkFileName name of the file containing the stylesheet
-	 * 						that will perform the checking
-	 * @throws ParserConfigurationException 
-	 * @throws IOException 
-	 * @throws SAXException 
-	 */
-	private MetadataCheck(String inputFileName)
-		throws SAXException, IOException, ParserConfigurationException
-	{
-		/*
-		 * Create a transformer factory.
-		 */
-		tFactory = TransformerFactory.newInstance();
-		
-		/*
-		 * Create a document builder factory.
-		 */
-		dbFactory = DocumentBuilderFactory.newInstance();
-		dbFactory.setNamespaceAware(true);
-		// dbFactory.setValidating(true);
-		
-		/*
-		 * Build documents for each of the input files.
-		 */
-		this.inputFileName = inputFileName;
-		inputDoc = buildDocument(inputFileName);
-	}
-	
-	/**
-	 * Command-line entry point.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		/*
-		 * Parse command-line arguments.
-		 */
-		if (args.length < 2) {
-			System.err.println("Usage: MetadataCheck infile checkfile...");
-			System.exit(1);
-		}
-		
-		MetadataCheck mdc;
-		try {
-			mdc = new MetadataCheck(args[0]);
-			for (int i = 1; i < args.length; i++) {
-				mdc.addChecker(args[i]);
-			}
-			mdc.run();
-		} catch (SAXException e) {
-			croak("SAX exception creating MetadataCheck", e);
-		} catch (IOException e) {
-			croak("I/O exception creating MetadataCheck", e);
-		} catch (ParserConfigurationException e) {
-			croak("parser configuration exception creating MetadataCheck", e);
-		}
-	}
+        public void fatalError(TransformerException exception)
+                throws TransformerException {
+            recordError(exception);
+        }
+
+        public void warning(TransformerException exception)
+                throws TransformerException {
+            recordError(exception);
+        }
+        
+    }
+    
+    private Document buildDocument(String fileName)
+        throws SAXException, IOException, ParserConfigurationException {
+        DocumentBuilder db = dbFactory.newDocumentBuilder();
+        return db.parse(fileName);
+    }
+    
+    private void performCheck(Document input, String inputFileName, Checker checker)
+        throws TransformerException {
+        Transformer t = tFactory.newTransformer(checker.getSource());
+        t.setErrorListener(new MyErrorListener(inputFileName, checker.getName()));
+        t.transform(new DOMSource(input), new DOMResult());
+    }
+    
+    public void run() {
+        try {
+            for (Checker check: checkers) {
+                performCheck(inputDoc, inputFileName, check);
+            }
+        } catch (TransformerConfigurationException e) {
+            croak("transformer configuration exception", e);
+        } catch (TransformerException e) {
+            croak("transformer exception", e);
+        }
+        if (errors != 0) {
+            System.err.println("*** ERRORS ENCOUNTERED IN " +
+                    inputFileName + " ***");
+            System.exit(1);
+        }
+    }
+
+    private void addChecker(String checkFileName) throws SAXException, IOException, ParserConfigurationException {
+        checkers.add(new Checker(checkFileName));
+    }
+    
+    /**
+     * Command-line entry point.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        /*
+         * Parse command-line arguments.
+         */
+        if (args.length < 2) {
+            System.err.println("Usage: MetadataCheck infile checkfile...");
+            System.exit(1);
+        }
+        
+        MetadataCheck mdc;
+        try {
+            mdc = new MetadataCheck(args[0]);
+            for (int i = 1; i < args.length; i++) {
+                mdc.addChecker(args[i]);
+            }
+            mdc.run();
+        } catch (SAXException e) {
+            croak("SAX exception creating MetadataCheck", e);
+        } catch (IOException e) {
+            croak("I/O exception creating MetadataCheck", e);
+        } catch (ParserConfigurationException e) {
+            croak("parser configuration exception creating MetadataCheck", e);
+        }
+    }
 
 }
